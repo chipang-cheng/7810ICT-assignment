3e0676621bc84c3322426bf8eac6822a704d4e42
chipang_cheng <chipang.cheng@griffithuni.edu.au>
2023-09-24 18:53:56 +1000

    modified logic for generating chart

--- assignment2.py
+++ assignment2.py
@@ -5,11 +5,10 @@ import wx.grid
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from temp import mainFrame as mainFrame1
+from noname import mainFrame as mainFrame1
 
 
 class MyFrame(mainFrame1):
-    column_mapping = {}
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -62,7 +61,6 @@ class MyFrame(mainFrame1):
                 self.m_grid1.DeleteCols(0, self.m_grid1.GetNumberCols())
                 self.m_grid1.AppendRows(len(self.df))
                 self.m_grid1.AppendCols(len(self.df.columns))
-
                 for i, col_name in enumerate(self.df.columns):
                     self.m_grid1.SetColLabelValue(i, col_name)
 
@@ -70,13 +68,11 @@ class MyFrame(mainFrame1):
                     for j, cell in enumerate(row):
                         self.m_grid1.SetCellValue(i, j, str(cell))
                     self.UpdateGauge(i + 1, rowsnum)
-
                 self.AddControlsForColumns(self.df.columns)
                 self.UpdateStatistics()
                 self.first_load = False
                 self.showLayout()
                 self.log_action(f"Loaded CSV file: {file_path}")
-
             else:
                 wx.LogError("The CSV file is empty.")
         except Exception as e:
@@ -92,7 +88,6 @@ class MyFrame(mainFrame1):
         for col_index, col_name in enumerate(columns):
             if col_name in ['OBJECTID', 'ACCIDENT_NO']:
                 continue
-
             checkbox = wx.CheckBox(self.m_scrolledWindow3, col_index * 10, "", wx.DefaultPosition, wx.DefaultSize, 0)
             self.grid_sizer.Add(checkbox, 0, wx.ALL, 5)
             self.checkboxes.append(checkbox)
@@ -104,9 +99,7 @@ class MyFrame(mainFrame1):
             self.grid_sizer.Add(text_ctrl, 1, wx.ALL | wx.EXPAND, 5)
             self.text_controls.append(text_ctrl)
             self.Bind(wx.EVT_CHECKBOX, self.OnCheckboxChange, checkbox)
-
         self.m_scrolledWindow3.SetSizer(self.grid_sizer)
-
         self.Layout()
 
     def OnCheckboxChange(self, event):
@@ -123,14 +116,12 @@ class MyFrame(mainFrame1):
             if checkbox.GetValue():
                 selected_fields.append(text_ctrl.GetId() // 10)  # Extract the column index
                 search_texts.append(text_ctrl.GetValue())
-
         if not selected_fields:
             filtered_data = self.df.copy()
             columns_to_display = list(self.df.columns)
         else:
             filtered_data = self.df.copy()
             columns_to_display = [self.df.columns[field] for field in selected_fields]
-
             for field, search_text in zip(selected_fields, search_texts):
                 if search_text:
                     field_name = self.df.columns[field]
@@ -189,12 +180,9 @@ class MyFrame(mainFrame1):
     def CalculateStatistics(self):
         if hasattr(self, 'df'):
             stats_data = []
-
             num_cols = self.m_grid1.GetNumberCols()
-
             selected_fields = [text_ctrl.GetId() // 10 for text_ctrl in self.text_controls if text_ctrl.GetValue()]
             filtered_data = self.df.copy()
-
             if selected_fields:
                 columns_to_display = [self.df.columns[field] for field in selected_fields]
                 for field, search_text in zip(selected_fields,
@@ -203,7 +191,6 @@ class MyFrame(mainFrame1):
                         field_name = self.df.columns[field]
                         filtered_data = filtered_data[
                             filtered_data[field_name].str.contains(search_text, case=False, na=False)]
-
                 for col_index in range(num_cols):
                     col_label = self.m_grid1.GetColLabelValue(col_index)
                     if col_label not in ['OBJECTID', 'ACCIDENT_NO'] and col_label in columns_to_display:
@@ -216,17 +203,14 @@ class MyFrame(mainFrame1):
                                 stats_data.append(
                                     {'Header': header, 'Value': val, 'Count': count, 'Percentage': percentage})
             stats = pd.DataFrame(stats_data)
-
             return stats
         return None
 
     def GetUniqueColumnValues(self, col_index):
         unique_values = {}
-
         if 0 <= col_index < self.m_grid1.GetNumberCols():
             selected_fields = [text_ctrl.GetId() // 10 for text_ctrl in self.text_controls if text_ctrl.GetValue()]
             filtered_data = self.df.copy()
-
             if selected_fields:
                 for field, search_text in zip(selected_fields,
                                               [text_ctrl.GetValue() for text_ctrl in self.text_controls]):
@@ -234,7 +218,6 @@ class MyFrame(mainFrame1):
                         field_name = self.df.columns[field]
                         filtered_data = filtered_data[
                             filtered_data[field_name].str.contains(search_text, case=False, na=False)]
-
             for row_index in range(self.m_grid1.GetNumberRows()):
                 if 0 <= row_index < self.m_grid1.GetNumberRows():
                     cell_value = self.m_grid1.GetCellValue(row_index, col_index)
@@ -244,19 +227,16 @@ class MyFrame(mainFrame1):
             total_count = sum(val_data['Count'] for val_data in unique_values.values())
             for key, val_data in unique_values.items():
                 val_data['Percentage'] = round((val_data['Count'] / total_count) * 100, 2)
-
         return unique_values
 
     def GetColumnValueCount(self, col_index, value):
         count = 0
-
         if 0 <= col_index < self.m_grid1.GetNumberCols():
             for row_index in range(self.m_grid1.GetNumberRows()):
                 if 0 <= row_index < self.m_grid1.GetNumberRows():
                     cell_value = self.m_grid1.GetCellValue(row_index, col_index)
                     if cell_value == value:
                         count += 1
-
         return count
 
     def GenerateChart(self, event):
@@ -264,25 +244,21 @@ class MyFrame(mainFrame1):
         y_axis_dialog = wx.SingleChoiceDialog(self, "Select Y-axis column:", "Y-axis Selection", y_axis_choices)
         if y_axis_dialog.ShowModal() == wx.ID_OK:
             y_axis_choice = y_axis_dialog.GetStringSelection()
-            x_axis_choices = ["ACCIDENT_DATE", "ACCIDENT_TIME", "Both"]
-            x_axis_dialog = wx.SingleChoiceDialog(self, "Select X-axis:", "X-axis Selection",
-                                                  x_axis_choices)
+            x_axis_choices = ["ACCIDENT_DATE", "ACCIDENT_TIME"]
+            x_axis_dialog = wx.SingleChoiceDialog(self, "Select X-axis:", "X-axis Selection", x_axis_choices)
             if x_axis_dialog.ShowModal() == wx.ID_OK:
                 x_axis_choice = x_axis_dialog.GetStringSelection()
                 num_rows = self.m_grid1.GetNumberRows()
                 num_cols = self.m_grid1.GetNumberCols()
                 x_axis_col = None
                 y_axis_col = None
-
                 for col_index in range(num_cols):
                     col_label = self.m_grid1.GetColLabelValue(col_index)
                     if col_label == x_axis_choice:
                         x_axis_col = col_index
                     elif col_label == y_axis_choice:
                         y_axis_col = col_index
-
                 if x_axis_col is not None and y_axis_col is not None:
-                    # Create a DataFrame from the grid data
                     data = {'X': [], 'Y': []}
                     for row in range(num_rows):
                         try:
@@ -292,43 +268,66 @@ class MyFrame(mainFrame1):
                             data['Y'].append(y_value)
                         except Exception as e:
                             wx.LogError(f"Error processing row {row}: {str(e)}")
-
                     df = pd.DataFrame(data)
-                    if x_axis_choice == "ACCIDENT_DATE":
+                    if x_axis_choice == "ACCIDENT_DATE" and 'ACCIDENT_DATE' in self.df.columns:
                         date_values = pd.to_datetime(df['X'], format='%d/%m/%Y', errors='coerce')
                         date_diff = (date_values.max() - date_values.min()).days
+                        print(date_diff)
                         if date_diff < 1:
                             x_date_format = "%d/%m/%Y"
-                        elif date_diff < 30:
+                        elif date_diff <= 30:
                             x_date_format = "%d/%m"
-                        elif date_diff < 365:
+                        elif date_diff <= 365:
                             x_date_format = "%m/%Y"
                         else:
                             x_date_format = "%Y"
                         df['X'] = date_values.dt.strftime(x_date_format)
-                    grouped_data = df.groupby(['X', 'Y']).size().reset_index(name='Count')
-
-                    plt.figure(figsize=(12, 8))
-                    unique_y_values = grouped_data['Y'].unique()
-
-                    for y_value in unique_y_values:
-                        y_data = grouped_data[grouped_data['Y'] == y_value]
-                        plt.plot(y_data['X'], y_data['Count'], marker='o', label=f'{y_value}')
-                    plt.xlabel(x_axis_choice)
-                    plt.ylabel(f"{y_axis_choice} Count")
-                    plt.title(f"Count by {x_axis_choice} for {y_axis_choice}")
-                    plt.legend()
-                    plt.grid(True)
-                    plt.xticks(rotation=45)  # Rotate X-axis labels for better readability
-                    plt.tight_layout()
-                    plt.show()
-                    self.log_action(f"Generated chart for Y-axis: {y_axis_choice}, X-axis: {x_axis_choice}")
+                        grouped_data = df.groupby(['X', 'Y']).size().reset_index(name='Count')
+                        plt.figure(figsize=(12, 8))
+                        unique_y_values = grouped_data['Y'].unique()
+                        for y_value in unique_y_values:
+                            y_data = grouped_data[grouped_data['Y'] == y_value]
+                            plt.plot(y_data['X'], y_data['Count'], marker='o', label=f'{y_value}')
+                        plt.xlabel(x_axis_choice)
+                        plt.ylabel(f"{y_axis_choice} Count")
+                        plt.title(f"Count by {x_axis_choice} for {y_axis_choice}")
+                        plt.legend()
+                        plt.grid(True)
+                        plt.xticks(rotation=45)
+                        plt.tight_layout()
+                        plt.show()
+                        self.log_action(f"Generated chart for Y-axis: {y_axis_choice}, X-axis: {x_axis_choice}")
+                    elif x_axis_choice == "ACCIDENT_TIME" and 'ACCIDENT_DATE' in self.df.columns:
+                        date_values = pd.to_datetime(self.df['ACCIDENT_DATE'] + ' ' + df['X'],
+                                                     format='%d/%m/%Y %H:%M:%S',
+                                                     errors='coerce')
+                        df['Date'] = date_values.dt.date
+                        df['Time'] = date_values.dt.time
+                        df['Hour'] = date_values.dt.hour
+                        date_range = (df['Date'].max() - df['Date'].min()).days
+                        if date_range > 0:
+                            # Calculate the total count on each Y-axis value per hour
+                            df_grouped = df.groupby(['Hour', 'Y']).size().reset_index(name='Count')
+                            # Calculate the average count per hour
+                            df_grouped['Count'] = df_grouped.groupby('Hour')['Count'].transform(
+                                lambda x: x / date_range)
+                            plt.figure(figsize=(12, 8))
+                            for y_value in df_grouped['Y'].unique():
+                                y_data = df_grouped[df_grouped['Y'] == y_value]
+                                plt.plot(y_data['Hour'], y_data['Count'], marker='o', label=f'{y_value}')
+                            plt.xlabel("Hour")
+                            plt.ylabel(f"Average {y_axis_choice} Count")
+                            plt.title(f"Average Count by Hour for {y_axis_choice}")
+                            plt.legend()
+                            plt.grid(True)
+                            plt.xticks(range(24))
+                            plt.tight_layout()
+                            plt.show()
+                            self.log_action(f"Generated chart for Y-axis: {y_axis_choice}, X-axis: {x_axis_choice}")
                 else:
                     wx.LogError(f"X-axis or Y-axis column not found in the grid.")
-
-            x_axis_dialog.Destroy()
-
-        y_axis_dialog.Destroy()
+                x_axis_dialog.Destroy()
+            y_axis_dialog.Destroy()
 
     def UpdateGauge(self, current_value, max_value):
         if current_value >= max_value:
